eeb9618d6c88d71333c8731175275b80452582f4
Maxime Augier <max@xolus.net>
2011-11-09 17:05:34 +0100

    Modularize caesar breaker

---
+++ Caesar.pm
@@ -0,0 +1,81 @@
+#!/usr/bin/perl
+
+package Caesar ;
+
+use Markov;
+use Storable;
+
+sub new {
+
+    my $self;
+
+    open NAMES, '<names/fr/all'
+        or die "Can't read names: $!\n";
+
+    while(<NAMES>) {
+        chomp;
+        $self->{names}{uc $_}=1;
+    }
+    close NAMES;
+
+    $self->{matcher} = retrieve('names/fr/all.pl');
+
+    bless $self;
+}
+
+sub is_valid {
+    my $char = shift;
+    return (ord $char >= ord 'A' && ord $char <= ord 'Z');
+}
+
+sub safelog {
+    my $n = shift;
+    return log $n if ($n > 0);
+    return -50;
+        
+}
+
+sub caesar {
+	my ($shift, $str) = @_;
+	my @r;
+	for (split  //, uc $str) {
+
+        if (is_valid($_)) {
+    		push @r, chr (65 + (((ord $_) - 65 + $shift) % 26));
+        } else {
+            push @r, $_;
+        }
+	}
+	return (join "",@r);
+}
+
+sub break {
+
+    my ($self, $name) = @_;
+
+    my @answ = map { [$_, caesar($_,$name)] } (0 .. 25);
+
+    for (@answ) {
+        my $sum = 0;
+        for (split /[^A-Z]+/, uc $_->[1]) {
+            print STDERR "Testing [$_] Sum = $sum\n";
+            $sum += safelog($self->{matcher}->match([split //, lc]));
+        }
+        $_->[2] = [0, $sum];
+    }
+
+# known names
+    for (@answ) {
+        $_->[2] = [1,0] if $self->{names}{$_->[1]};
+    }
+
+    return sort { tuplecmp($b->[2], $a->[2]) } @answ;
+
+}
+
+sub tuplecmp {
+    my ($x,$y) = @_;
+    $x->[0] <=> $y->[0] || $x->[1] <=> $y->[1];
+}
+
+1;
--- tcaesar.pl
+++ tcaesar.pl
@@ -1,83 +1,16 @@
 #!/usr/bin/perl
 
 use Data::Dumper;
-use Markov;
-use Storable;
 use Term::ANSIColor;
+use Caesar;
 
-my %names;
-
-open NAMES, '<names/fr/all'
-    or die "Can't read names: $!\n";
-
-while(<NAMES>) {
-    chomp;
-    $names{uc $_}=1;
-}
-close NAMES;
-
-my $matcher = retrieve('names/fr/all.pl');
-
-sub is_valid {
-    my $char = shift;
-    return (ord $char >= ord 'A' && ord $char <= ord 'Z');
-}
-
-sub safelog {
-    my $n = shift;
-    return log $n if ($n > 0);
-    return -50;
-        
-}
-
-sub caesar {
-	my ($shift, $str) = @_;
-	my @r;
-	for (split  //, uc $str) {
-
-        if (is_valid($_)) {
-    		push @r, chr (65 + (((ord $_) - 65 + $shift) % 26));
-        } else {
-            push @r, $_;
-        }
-	}
-	return (join "",@r);
-}
+my $caesar = Caesar->new();
 
 print colored(['bold white'], `figlet <<< caesar`);
 
 print STDERR colored(['bold white'],"Entrez le nom: ");
 my $name = uc <>;
 
-chomp $name;
+print Dumper([ $caesar->break($name) ]);
 
-my @answ = map { caesar($_,$name) } (0 .. 25);
 
-print STDERR colored(['bold white'], "Possibilités:\n");
-
-print "$_\n" for @answ;
-
-<>;
-my %answ;
-
-# stat match
-for (@answ) {
-    my $sum = 0;
-    for (split /[^A-Z]+/, uc) {
-        print STDERR "Testing [$_] Sum = $sum\n";
-        $sum += safelog($matcher->match([split //, lc]));
-    }
-    $answ{$_} = [0, $sum];
-}
-
-# known names
-for (@answ) {
-    $answ{$_} = [1,0] if $names{$_};
-}
-
-sub tuplecmp {
-    my ($x,$y) = @_;
-    $x->[0] <=> $y->[0] || $x->[1] <=> $y->[1];
-}
-
-print "$_, $answ{$_}->[0], $answ{$_}->[1]\n" for sort { tuplecmp($answ{$b},$answ{$a}); } @answ;
