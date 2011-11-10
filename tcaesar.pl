224d5eb751ad0eb2e4084e9d23ee18b7da1e5c39
Maxime Augier <max@xolus.net>
2011-11-10 13:19:31 +0100

    tcaesar better

--- tcaesar.pl
+++ tcaesar.pl
@@ -3,14 +3,24 @@
 use Data::Dumper;
 use Term::ANSIColor;
 use Caesar;
+use Stat;
 
 my $caesar = Caesar->new();
 
-print colored(['bold white'], `figlet <<< caesar`);
+print colored(['bold cyan'], `figlet <<< caesar`);
 
-print STDERR colored(['bold white'],"Entrez le nom: ");
+print STDERR colored(['bold white'],"Entrez une phrase: ");
 my $name = uc <>;
 
-print Dumper([ $caesar->break($name) ]);
+my @break = $caesar->break($name);
+my $normer = Stat->new(map {$_->[2][1]} @break)->norm;
+
+for (@break) {
+	my $str = sprintf " %02d | % 8f | %s", $_->[0], $_->[2][1], $_->[1];
+	my $color = 'red';
+	$color = 'bold yellow' if $normer->($_->[2][1]) > 0.15;
+	$color = 'bold green' if $_->[2][0];
+	print colored([$color], $str);
+}
 
 
