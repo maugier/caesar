a9087e1544ed56c6be7ff507a75960631a3196fd
Maxime Augier <max@xolus.net>
2014-04-02 17:32:33 +0200

    Small interface tuning

--- tcaesar.pl
+++ tcaesar.pl
@@ -5,17 +5,19 @@ use Term::ANSIColor;
 use Caesar;
 use Stat;
 
+$| = 0;
+
 my $caesar = Caesar->new();
 
 print colored(['bold cyan'], `figlet <<< caesar`);
 
-print STDERR colored(['bold white'],"Entrez une phrase: ");
+print colored(['bold white'],"Entrez une phrase: ");
 my $name = uc <>;
 
 my @break = $caesar->break($name);
 my $normer = Stat->new(map {$_->[2][1]} @break)->norm;
 
-for (@break) {
+for (@break[0 .. 10]) {
 	my $str = sprintf " %02d | % 8f | %s", $_->[0], $_->[2][1], $_->[1];
 	my $color = 'red';
 	$color = 'bold yellow' if $normer->($_->[2][1]) > 0.15;
